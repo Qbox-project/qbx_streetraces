0a758efe7d8afe6dbca02d4b30aeb64b5c25f946
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-10-10 23:02:44 -0400

    chore: accommodate new core

--- client/main.lua
+++ client/main.lua
@@ -1,38 +1,22 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
 local Races = {}
 local InRace = false
 local RaceId = 0
 local ShowCountDown = false
 local RaceCount = 5
 
-local function DrawText3Ds(x, y, z, text)
-	SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
-
 local function RaceCountDown()
     ShowCountDown = true
     while RaceCount ~= 0 do
-        FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), true), true)
+        FreezeEntityPosition(GetVehiclePedIsIn(cache.ped, true), true)
         PlaySound(-1, "slow", "SHORT_PLAYER_SWITCH_SOUND_SET", 0, 0, 1)
-        QBCore.Functions.Notify(RaceCount, 'primary', 800)
+        exports.qbx_core:Notify(RaceCount, 'primary', 800)
         Wait(1000)
         RaceCount = RaceCount - 1
     end
     ShowCountDown = false
     RaceCount = 5
-    FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), true), false)
-    QBCore.Functions.Notify("GOOOOOOOOO!!!")
+    FreezeEntityPosition(GetVehiclePedIsIn(cache.ped, true), false)
+    exports.qbx_core:Notify("GOOOOOOOOO!!!")
 end
 
 CreateThread(function()
@@ -40,13 +24,13 @@ CreateThread(function()
         local sleep = 1000
         if Races ~= nil then
             -- No race yet
-            local pos = GetEntityCoords(PlayerPedId(), true)
+            local pos = GetEntityCoords(cache.ped, true)
             if RaceId == 0 then
                 for k in pairs(Races) do
                     if Races[k] ~= nil then
                         if #(pos - vector3(Races[k].startx, Races[k].starty, Races[k].startz)) < 15.0 and not Races[k].started then
                             sleep = 0
-                            DrawText3Ds(Races[k].startx, Races[k].starty, Races[k].startz, "[~g~H~w~] To Join The Race (~g~$"..Races[k].amount..",-~w~)")
+                            DrawText3D("[~g~H~w~] To Join The Race (~g~$"..Races[k].amount..",-~w~)", vec3(Races[k].startx, Races[k].starty, Races[k].startz))
                             if IsControlJustReleased(0, 74) then
                                 TriggerServerEvent("qb-streetraces:JoinRace", k)
                             end
@@ -59,14 +43,14 @@ CreateThread(function()
             if RaceId ~= 0 and not InRace then
                 if #(pos - vector3(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz)) < 15.0 and not Races[RaceId].started then
                     sleep = 0
-                    DrawText3Ds(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz, "Race Will Start Soon")
+                    DrawText3D("Race Will Start Soon", vec3(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz))
                 end
             end
             -- In race and started
             if RaceId ~= 0 and InRace then
                 if #(pos - vector3(Races[RaceId].endx, Races[RaceId].endy, pos.z)) < 250.0 and Races[RaceId].started then
                     sleep = 0
-                    DrawText3Ds(Races[RaceId].endx, Races[RaceId].endy, pos.z + 0.98, "FINISH")
+                    DrawText3D("FINISH", vec3(Races[RaceId].endx, Races[RaceId].endy, pos.z + 0.98))
                     if #(pos - vector3(Races[RaceId].endx, Races[RaceId].endy, pos.z)) < 15.0 then
                         TriggerServerEvent("qb-streetraces:RaceWon", RaceId)
                         InRace = false
@@ -77,7 +61,7 @@ CreateThread(function()
             if ShowCountDown then
                 if #(pos - vector3(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz)) < 15.0 and Races[RaceId].started then
                     sleep = 0
-                    DrawText3Ds(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz, "Race start in ~g~"..RaceCount)
+                    DrawText3D("Race start in ~g~"..RaceCount, vec3(Races[RaceId].startx, Races[RaceId].starty, Races[RaceId].startz))
                 end
             end
         end
@@ -97,7 +81,7 @@ RegisterNetEvent('qb-streetraces:RaceDone', function(race, winner)
     if RaceId ~= 0 and RaceId == race then
         RaceId = 0
         InRace = false
-        QBCore.Functions.Notify("Race Is Over! The Winner Is "..winner.. "!")
+        exports.qbx_core:Notify("Race Is Over! The Winner Is "..winner.."!")
     end
 end)
 
@@ -107,7 +91,7 @@ RegisterNetEvent('qb-streetraces:StopRace', function()
 end)
 
 RegisterNetEvent('qb-streetraces:CreateRace', function(amount)
-    local pos = GetEntityCoords(PlayerPedId(), true)
+    local pos = GetEntityCoords(cache.ped, true)
     local WaypointHandle = GetFirstBlipInfoId(8)
     if DoesBlipExist(WaypointHandle) then
         local c = GetBlipInfoIdCoord(WaypointHandle)
@@ -126,12 +110,12 @@ RegisterNetEvent('qb-streetraces:CreateRace', function(amount)
                 joined = {}
             }
             TriggerServerEvent("qb-streetraces:NewRace", race)
-            QBCore.Functions.Notify("Race Made For $"..amount.."", "success")
+            exports.qbx_core:Notify("Race Made For $"..amount.."", "success")
         else
-            QBCore.Functions.Notify("End Position Is Too Close", "error")
+            exports.qbx_core:Notify("End Position Is Too Close", "error")
         end
     else
-        QBCore.Functions.Notify("You Need To Drop A Marker", "error")
+        exports.qbx_core:Notify("You Need To Drop A Marker", "error")
     end
 end)
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-StreetRaces'
+description 'QBX-StreetRaces'
+repository 'https://github.com/Qbox-project/qbx_streetraces'
 version '1.0.0'
 
+shared_scripts {'@ox_lib/init.lua', '@qbx_core/import.lua'}
 client_script 'client/main.lua'
-server_script'server/main.lua'
+server_script 'server/main.lua'
+
+modules {'qbx_core:utils'}
 
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
