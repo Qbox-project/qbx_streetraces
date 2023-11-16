0706de7c2a14bef7388a2cde23c90c7be76c29ab
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:41:53 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-StreetRaces'
+description 'QBX_StreetRaces'
 repository 'https://github.com/Qbox-project/qbx_streetraces'
 version '1.0.0'
 
-shared_scripts {'@ox_lib/init.lua', '@qbx_core/import.lua'}
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/utils.lua'
+
+}
 client_script 'client/main.lua'
 server_script 'server/main.lua'
-
-modules {'qbx_core:utils'}
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
